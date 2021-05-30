506f72114b5bbdd3bee958a3e93018ad60e1cb7f
anseunghun <size411@naver.com>
2021-05-30 19:10:26 +0900

    gradle downgrade & TDD test(lombok, jsonPath)

---
+++ src/test/java/com/jojoldu/book/springboot/dto/HelloResponseDtoTest.java
@@ -0,0 +1,20 @@
+package com.jojoldu.book.springboot.dto;
+
+import com.jojoldu.book.springboot.web.dto.HelloResponseDto;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HelloResponseDtoTest {
+
+    @Test
+    public void lombokFunctionTest() {
+        String name = "test";
+        int amount = 1000;
+
+        HelloResponseDto dto = new HelloResponseDto(name, amount);
+
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+    }
+}
