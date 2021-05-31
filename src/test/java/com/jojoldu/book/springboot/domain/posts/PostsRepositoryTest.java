d27d49268ab7198b966dfbe31e70d19878d9dfb9
anseunghun <size411@naver.com>
2021-05-31 23:40:32 +0900

    Create JPA Repository, H2 DB CR Test

---
+++ src/test/java/com/jojoldu/book/springboot/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,46 @@
+package com.jojoldu.book.springboot.domain.posts;
+
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PostsRepositoryTest {
+    @Autowired
+    PostsRepository postsRepository;
+
+    @After
+    public void cleanup() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void saveLoadPost() {
+        // given
+        String title = "Test Post";
+        String content = "Test Content";
+
+        postsRepository.save(Posts.builder()
+                                  .title(title)
+                                  .content(content)
+                                  .author("powerman@gmail.com")
+                                .build());
+
+        // when
+        List<Posts> postList = postsRepository.findAll();
+
+        // then
+        Posts posts = postList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+    }
+}
