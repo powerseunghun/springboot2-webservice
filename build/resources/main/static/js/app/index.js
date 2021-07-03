93c70c4524bc6ca7195588cbbcf36690a55db8a1
anseunghun <size411@naver.com>
2021-07-03 21:46:05 +0900

    Delete Function Implements & Test

--- build/resources/main/static/js/app/index.js
+++ build/resources/main/static/js/app/index.js
@@ -7,6 +7,9 @@ var main = {
         $('#btn-update').on('click', function() {
             _this.update();
         });
+        $('#btn-delete').on('click', function() {
+            _this.delete();
+        });
     },
     save: function() {
         var data = {
@@ -14,7 +17,6 @@ var main = {
             author: $('#author').val(),
             content: $('#content').val()
         };
-        alert(data);
         $.ajax({
             type: 'POST',
             url: '/api/v1/posts',
@@ -43,6 +45,21 @@ var main = {
             data: JSON.stringify(data)
         }).done(function() {
             alert('글이 수정되었습니다.');
+            window.location.href = '/';
+        }).fail(function(error) {
+            alert(JSON.stringify(error));
+        });
+    },
+    delete: function() {
+        var id = $('#id').val();
+
+        $.ajax({
+            type: 'DELETE',
+            url: '/api/v1/posts/' + id,
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8'
+        }).done(function() {
+            alert('글이 삭제되었습니다.');
             window.location.href = '/';
         }).fail(function(error) {
             alert(JSON.stringify(error));
