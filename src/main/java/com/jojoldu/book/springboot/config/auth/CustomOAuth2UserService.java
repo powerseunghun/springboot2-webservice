4abbd9413a7b5b44f1421abbb515a9a4ea22c141
anseunghun <size411@naver.com>
2021-07-13 00:18:05 +0900

    Create Custom OAuth2UserLoginService for google api

---
+++ src/main/java/com/jojoldu/book/springboot/config/auth/CustomOAuth2UserService.java
@@ -0,0 +1,42 @@
+package com.jojoldu.book.springboot.config.auth;
+
+import com.jojoldu.book.springboot.config.auth.dto.OAuthAttributes;
+import com.jojoldu.book.springboot.domain.user.User;
+import com.jojoldu.book.springboot.domain.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpSession;
+
+@RequiredArgsConstructor
+@Service
+public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
+    private final UserRepository userRepository;
+    private final HttpSession httpSession;
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        OAuth2UserService<OAuth2UserRequest, OAuth2User> delegate = new DefaultOAuth2UserService();
+        OAuth2User oAuth2User = delegate.loadUser(userRequest);
+
+        String registrationId = userRequest.getClientRegistration().getRegistrationId();
+        String userNameAttributeName = userRequest
+                .getClientRegistration().getProviderDetails()
+                .getUserInfoEndpoint()
+                .getUserNameAttributeName();
+
+        OAuthAttributes attributes = OAuthAttributes
+                .of(registrationId, userNameAttributeName,
+                        oAuth2User.getAttributes());
+
+        User user = saveOrUpdate(attributes);
+
+//        httpSession.setAttribute("user", new SessionUser);
+        return null;
+    }
+}
---
+++ src/main/java/com/jojoldu/book/springboot/config/auth/dto/SessionUser.java
@@ -0,0 +1,5 @@
+package com.jojoldu.book.springboot.config.auth.dto;
+
+public class SessionUser {
+
+}
