abe9225f199af50b245e8cff58972d3d5f823e31
anseunghun <size411@naver.com>
2021-07-13 20:39:17 +0900

    Add Function OAuth2 Google Login

--- src/main/java/com/jojoldu/book/springboot/config/auth/SecurityConfig.java
+++ src/main/java/com/jojoldu/book/springboot/config/auth/SecurityConfig.java
@@ -4,10 +4,11 @@ import com.jojoldu.book.springboot.domain.user.Role;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @RequiredArgsConstructor
 @EnableWebSecurity
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private final CustomOAuth2UserService customOAuth2UserService;
 
     protected void configure(HttpSecurity http) throws Exception {
--- src/main/java/com/jojoldu/book/springboot/config/auth/dto/SessionUser.java
+++ src/main/java/com/jojoldu/book/springboot/config/auth/dto/SessionUser.java
@@ -3,8 +3,10 @@ package com.jojoldu.book.springboot.config.auth.dto;
 import com.jojoldu.book.springboot.domain.user.User;
 import lombok.Getter;
 
+import java.io.Serializable;
+
 @Getter
-public class SessionUser {
+public class SessionUser implements Serializable {
     private String name;
     private String email;
     private String picture;
